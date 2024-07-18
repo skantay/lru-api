4e2b01e6d43ffc2f1f367b4424d853b63a50d4be
sanzhar <skantay03@gmail.com>
2024-07-18 14:31:44 +0500

    changed type for defaultCacheTTL flag

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -9,14 +9,14 @@ import (
 type Config struct {
 	HTTPPort        string `env:"HTTP_PORT" envDefault:"8080"`
 	CacheSize       uint   `env:"CACHE_SIZE" envDefault:"10"`
-	DefaultCacheTTL uint   `env:"DEFAULT_CACHE_TTL" envDefault:"60"`
+	DefaultCacheTTL int64  `env:"DEFAULT_CACHE_TTL" envDefault:"60"`
 	LogLevel        string `env:"LOG_LEVEL" envDefault:"WARN"`
 }
 
 func LoadConfig() (*Config, error) {
 	httpPort := flag.String("server-host-port", "", "HTTP port")
 	cacheSize := flag.Uint("cache-size", 0, "Cache size")
-	defaultCacheTTL := flag.Uint("default-cache-ttl", 0, "Default cache TTL")
+	defaultCacheTTL := flag.Int64("default-cache-ttl", 0, "Default cache TTL")
 	logLevel := flag.String("log-level", "", "Log level")
 
 	flag.Parse()
